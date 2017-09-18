d3b6bba67c7cbbbf7917c8a9ff5b0bdc5ec9d2bb
mmezzanotte <mmezzanotte@temperies.com>
2017-09-18 13:08:36 -0300

    organizacion de carpetas

---
+++ view/groceryView.php
@@ -0,0 +1,26 @@
+<?php
+require('../libs/smarty-3.1.30/libs/Smarty.class.php');
+class GroceryView
+{
+  private $smarty;
+  function __construct()
+  {
+    $this->smarty = new Smarty();
+  }
+  function agregarError($error){
+    $this->smarty->assign('error',$error);
+  }
+  function mostrarMensaje($mensaje, $tipo){
+  //  $this->smarty->assign('mensaje',$mensaje);
+  //  $this->smarty->assign('tipoMensaje',$tipo);
+  }
+  function mostrar($tareas){
+  //  $this->smarty->assign('tareas',$tareas);
+  //  $this->smarty->display('index.tpl');
+  }
+  function getLista($tareas){
+  //  $this->smarty->assign('tareas',$tareas);
+  //  $this->smarty->display('lista.tpl');
+  }
+}
+ ?>
